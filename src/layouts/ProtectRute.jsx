cd3ca04c2521a15be59c39ea4ed360fc60f7b464
gacm952 <personalaccounts@gacm952.com>
2023-07-22 11:50:14 -0500

    update

--- src/layouts/ProtectRute.jsx
+++ src/layouts/ProtectRute.jsx
@@ -1,28 +1,20 @@
-import { Outlet, useNavigate } from "react-router-dom"
-import useAuth from "../hooks/useAuth"
+import { Outlet, useNavigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 const ProtectRute = () => {
+  const { auth, loading } = useAuth();
+  const navigate = useNavigate();
 
-    const { auth, loading } = useAuth()
-    const navigate = useNavigate()
+  if (loading) {
+    return 'Cargando...';
+  }
 
-    if (loading) {
-        return 'Cargando...'
-    }
+  return (
+    <>
+      {auth?.role === "User" && <Outlet />}
+      {!auth && navigate('/')}
+    </>
+  );
+};
 
-    return (
-        <>
-
-        {auth.role === "User" ? 
-        (
-
-         <Outlet/>
-                         
-        ) : navigate('/') }
-
-        </>
-    )
-}
-
-
-export default ProtectRute
+export default ProtectRute;
--- src/layouts/ProtectRuteAdmission.jsx
+++ src/layouts/ProtectRuteAdmission.jsx
@@ -2,18 +2,27 @@ import { Outlet, useNavigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const ProtectRuteAdmission = () => {
   const { auth, loading } = useAuth();
   const navigate = useNavigate();
+  const [isAuthReady, setIsAuthReady] = useState(false);
 
-  if (loading) {
+  useEffect(() => {
+    if (!loading) {
+      setIsAuthReady(true);
+    }
+  }, [loading]);
+
+  if (loading || !isAuthReady) {
     return 'Cargando...';
   }
 
   return (
     <>
-      {auth.role === "Admission" ?  (
+      {auth.role === "Admission" ? (
         <div className="w-full hidden sm:block min-w-600">
           <div className="flex">
             <Header />
